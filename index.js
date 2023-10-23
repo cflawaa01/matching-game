14520b30dfbf5274dfec55c491560f00e3788492
chung <cflawaa01@gmail.com>
2023-10-23 23:31:47 +0100

    add flip function

--- index.js
+++ index.js
@@ -3,26 +3,31 @@ const mycards = [
     {name:"chick",img:"chick.png"},
     {name:"ghost",img:"ghost.png"},
     {name:"postman",img:"postman.png"},
-    {name:"raphael",img:"raphel.png"}
+    {name:"raphael",img:"raphael.png"}
 ]
 let gamearray = [
     {name:"angry",img:"angry.png"},
     {name:"chick",img:"chick.png"},
     {name:"ghost",img:"ghost.png"},
     {name:"postman",img:"postman.png"},
-    {name:"raphael",img:"raphel.png"}
+    {name:"raphael",img:"raphael.png"}
 ]
 for(i=0;i<3;i++){
  gamearray.push(mycards[Math.floor(Math.random()*5)])
 }
 const newarray = gamearray.flatMap(i => [i,i]); 
-console.log(newarray)
+newarray.sort(()=>0.5-Math.random());
 function makeABoard(){
     for(i=0;i<newarray.length;i++){
         const carddisplay = document.createElement("img");
         carddisplay.setAttribute("src","./public/question-mark.png");
         carddisplay.setAttribute("data-id",i);
         document.querySelector(".img-container").appendChild(carddisplay);
+        carddisplay.addEventListener("click",flip)
     }
 }
 makeABoard();
+function flip(){
+    document.querySelector(`[data-id="${this.getAttribute("data-id")}"]`).src = "./public/"+`${newarray[this.getAttribute("data-id")].img}`
+
+}
